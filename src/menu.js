d0fd3b46e31137289f7b4950113b0b1e0ece50b2
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2023-09-17 11:01:44 +0300

    Improve menu page

--- src/menu.js
+++ src/menu.js
@@ -3,28 +3,69 @@ const loadMenu = () => {
   const menuSection = document.createElement('section');
   menuSection.classList.add('menu');
   
-  // Create menu items
-  const menuItem1 = document.createElement('div');
-  const menuItem2 = document.createElement('div');
-  menuItem1.classList.add('menu-item', 'appetizers');
-  menuItem2.classList.add('menu-item', 'main-courses');
-  menuItem1.innerHTML = `
-  <h3>Appetizers</h3>
-  <ul>
-    <li>Appetizer 1 - $10.99</li>
-    <li>Appetizer 2 - $9.99</li>
-  </ul>
-  `;
-  menuItem2.innerHTML = `
-  <h3>Main Courses</h3>
-  <ul>
-    <li>Main Course 1 - $10.99</li>
-    <li>Main Course 2 - $9.99</li>
-  </ul>`;
+  // Define menu data (you can replace this with your actual menu data)
+  const menuData = [
+    {
+      category: 'Appetizers',
+      items: [
+        { name: 'Appetizer 1', description: 'Description of Appetizer 1', price: '$10.99', imageSrc: 'appetizer1.jpg' },
+        { name: 'Appetizer 2', description: 'Description of Appetizer 2', price: '$9.99', imageSrc: 'appetizer2.jpg' }
+      ]
+    },
+    {
+      category: 'Main Courses',
+      items: [
+        { name: 'Main Course 1', description: 'Description of Main Course 1', price: '$10.99', imageSrc: 'maincourse1.jpg' },
+        { name: 'Main Course 2', description: 'Description of Main Course 2', price: '$9.99', imageSrc: 'maincourse2.jpg' }
+      ]
+    }
+  ];
 
-  // Append menu items to menu section
-  menuSection.appendChild(menuItem1);
-  menuSection.appendChild(menuItem2);
+  // Loop through menu data and create elements
+  menuData.forEach(categoryData => {
+    const categoryElement = document.createElement('div');
+    categoryElement.classList.add('menu-category');
+    
+    const categoryTitle = document.createElement('h3');
+    categoryTitle.textContent = categoryData.category;
+    
+    categoryElement.appendChild(categoryTitle);
+    
+    categoryData.items.forEach(itemData => {
+      const menuItem = document.createElement('div');
+      menuItem.classList.add('menu-item');
+      
+      const itemImage = document.createElement('img');
+      itemImage.src = itemData.imageSrc;
+      itemImage.alt = itemData.name;
+      
+      const itemInfo = document.createElement('div');
+      itemInfo.classList.add('menu-item-info');
+      
+      const itemName = document.createElement('p');
+      itemName.classList.add('menu-item-name');
+      itemName.textContent = itemData.name;
+      
+      const itemDescription = document.createElement('p');
+      itemDescription.classList.add('menu-item-description');
+      itemDescription.textContent = itemData.description;
+      
+      const itemPrice = document.createElement('p');
+      itemPrice.classList.add('menu-item-price');
+      itemPrice.textContent = itemData.price;
+      
+      itemInfo.appendChild(itemName);
+      itemInfo.appendChild(itemDescription);
+      itemInfo.appendChild(itemPrice);
+      
+      menuItem.appendChild(itemImage);
+      menuItem.appendChild(itemInfo);
+      
+      categoryElement.appendChild(menuItem);
+    });
+    
+    menuSection.appendChild(categoryElement);
+  });
 
   // Clear content container
   const contentContainer = document.getElementById('content');
@@ -34,6 +75,6 @@ const loadMenu = () => {
   
   // Append menu section to content container
   contentContainer.appendChild(menuSection);
-}
+};
 
 export default loadMenu;
